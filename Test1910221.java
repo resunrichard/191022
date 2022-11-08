569081362a7ff1daf248108cfc0df45c51d237fe
resunrichard <116341273+resunrichard@users.noreply.github.com>
2022-11-08 12:39:06 +0530

    Add files via upload

---
+++ Test1910221.java
@@ -0,0 +1,11 @@
+//Sample java program:
+//----------------------------
+public class Test1910221 {
+static int a=100; //static variable
+void m() {
+int c=25; //local variable
+}
+public static void main(String args[]) {
+int b=75; instance variable
+}
+}
