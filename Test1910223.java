e7e2fc1b624165b29cfd0e0dbe9fff53ca0142ad
resunrichard <116341273+resunrichard@users.noreply.github.com>
2022-11-08 12:39:58 +0530

    Add files via upload

---
+++ Test1910223.java
@@ -0,0 +1,9 @@
+// Example for implicit converation
+public class Test1910223{
+public static void main(String args[]) {
+	float f=29.30f; // 4 byes of memory
+	double d=f; // 8 byes of memory
+	System.out.println(f);//29.30
+	System.out.println(d);//29.3000000
+}
+}
