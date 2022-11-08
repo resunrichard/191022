36f7ed50b3b8031a1ae976049b6d63f20889ec3f
resunrichard <116341273+resunrichard@users.noreply.github.com>
2022-11-08 12:39:32 +0530

    Add files via upload

---
+++ Test1910222.java
@@ -0,0 +1,21 @@
+//Example primitive data types declaration
+public class Test{
+public static void main(String args[]) {
+	boolean one=true;
+	byte a=10;
+	short s=1000;
+	char ch='A';
+	long l=1000000L;
+	int i=25;
+	float f=29.30f;
+	double d=23.84;
+	System.out.println(one);//true
+	System.out.println(a);//10
+	System.out.println(s);//1000
+	System.out.println(ch);//A
+	System.out.println(l);//100000
+	System.out.println(i);//25
+	System.out.println(f);//29.30
+	System.out.println(d);//23.84
+}
+}
